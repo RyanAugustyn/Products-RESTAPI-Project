fb1ce6fd1fbb59ba083be44d7d66d7c55722bc3d
Ryan Augustyn <jraugustyn07@gmail.com>
2023-03-10 14:37:48 -0600

    added get, get_by_id, and post functions to respective urls

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
+from marshmallow import post_load, fields, ValidationError
 from dotenv import load_dotenv
 from os import environ
 
@@ -30,24 +31,37 @@ class Product(db.Model):
     price=db.Column(db.Float)
     inventory_quantity=db.Column(db.Integer)
 
-
-
 # Schemas
 class ProductSchema(ma.Schema):
-    #id=fields.Integer(primary_key=True)
-    pass
+    id=fields.Integer(primary_key=True)
+    name=fields.String()
+    description=fields.String()
+    price=fields.Float()
+    inventory_qualtity=fields.Integer()
+    class Meta:
+        fields = ("id", "name", "description", "price", "inventory_quantity")
+    @post_load
+    def create_product(self, data, **kwargs):
+        return Product(**data)
 
+product_schema=ProductSchema()
+products_schema=ProductSchema(many=True)    
 
 # Resources
 class ProductListResource(Resource):
     def get(self):
         all_products=Product.query.all()
+        return products_schema.dump(all_products), 200
+    def post(self):
+        form_data=request.get_json
 
 
 class ProductResource(Resource):
-    pass
+    def get_by_id(self, product_id):
+        product=Product.query.get_or_404(product_id)
+        return product_schema.dump(product), 200
 
 
 # Routes
-api.add_resource(ProductListResource, 'api/products')
-api.add_resource(ProductResource, 'api/products/<int:pk>')
+api.add_resource(ProductListResource, '/api/products')
+api.add_resource(ProductResource, '/api/products/<int:pk>')
